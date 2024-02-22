395f277a20e662f3e6eb0b2ad16c81fd2c85d41e
Clipertrucado <145342712+Clipertrucado@users.noreply.github.com>
2024-02-22 09:07:33 +0100

    Add files via upload

---
+++ ObligatorioLM/src/main/webapp/ReadMe.js
@@ -0,0 +1,34 @@
+document.write("Caso 1: ");
+
+var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);
+  
+
+document.write(b);
+}
+
+document.write(a);
+document.write(b);
+
+document.write("Caso 2: ");
+
+for (let i = 0; i < 10; i++) {
+  document.write(i);
+}
+
+//cument.write(i);
+
+document.write("Caso 3: ");
+
+var z = 5
+    let x = z+10, y = 12;
+   
+    document.write(x + y);
